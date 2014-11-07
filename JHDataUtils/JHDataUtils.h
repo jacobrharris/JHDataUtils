79d2e6cfdb99f9d87a58fdcf0e3dff1c590451b7
Jacob Harris <jacob@jacobharris.org>
2014-11-06 16:50:43 -0800

    Initial Commit

---
+++ JHDataUtils/JHDataUtils.h
@@ -0,0 +1,13 @@
+//
+//  JHDataUtils.h
+//  JHDataUtils
+//
+//  Created by Jacob Harris on 11/6/14.
+//  Copyright (c) 2014 Jacob Harris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JHDataUtils : NSObject
+
+@end
---
+++ JHDataUtils/JHDataUtils.m
@@ -0,0 +1,13 @@
+//
+//  JHDataUtils.m
+//  JHDataUtils
+//
+//  Created by Jacob Harris on 11/6/14.
+//  Copyright (c) 2014 Jacob Harris. All rights reserved.
+//
+
+#import "JHDataUtils.h"
+
+@implementation JHDataUtils
+
+@end
